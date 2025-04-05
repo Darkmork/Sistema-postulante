664fb74fdd4c356cd516b398721cb71e88ce9c2b
Darkmork <jorge.gangale@mail.udp.cl>
2025-04-04 23:46:10 -0300

    Mi primer commit

---
+++ src/main/java/cl/colegiomtn/sistpostulante/SistPostulanteApplication.java
@@ -0,0 +1,13 @@
+package cl.colegiomtn.sistpostulante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistPostulanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistPostulanteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/colegiomtn/sistpostulante/SistPostulanteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.colegiomtn.sistpostulante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistPostulanteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
